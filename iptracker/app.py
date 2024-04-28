6e6aed1acff6234b34948ae1bc06bbe02cc5761a
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-04-28 18:18:24 +0200

    Implement main app

---
+++ iptracker/app.py
@@ -0,0 +1,67 @@
+import json
+import os
+from flask import Flask, request
+from pymongo import MongoClient
+from iptracker.api import IPAPI, response_to_dict
+from iptracker.db import HostDataStore
+from iptracker.resolver import HostResolver
+
+MONGO_URI = os.getenv("MONGO_URI")
+CACHE_EXPIRATION_TIME = os.getenv("CACHE_EXPIRATION_TIME")
+COLLECTED_FIELDS = os.getenv("COLLECTED_FIELDS")
+COLLECTED_FIELDS = COLLECTED_FIELDS.split(",") if COLLECTED_FIELDS else None
+IPAPI_USER_AGENT = os.getenv("USER_AGENT")
+IPAPI_URL = os.getenv("IPAPI_URL")
+IPAPI_BATCH_SIZE = os.getenv("IPAPI_BATCH_SIZE")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+LISTEN_ADDRESS = os.getenv("LISTEN_ADDRESS", "0.0.0.0")
+APP_PORT = int(os.getenv("APP_PORT", 8080))
+
+app = Flask(__name__)
+app.logger.setLevel(LOG_LEVEL)
+
+api = IPAPI(IPAPI_URL, IPAPI_BATCH_SIZE, IPAPI_USER_AGENT)
+ds = None
+
+if MONGO_URI:
+    connection = MongoClient(MONGO_URI)
+    ds = HostDataStore(ds, CACHE_EXPIRATION_TIME)
+else:
+    app.logger.warning("MongoDB URI not set. Queries will not be cached locally.")
+
+resolver = HostResolver(api, ds)
+
+@app.route("/json/<ip_address>", methods=["GET", "POST"])
+def endpoint_single(ip_address):
+    fields = request.args.get("fields", None)
+    fields = fields.split(",") if fields else COLLECTED_FIELDS
+    skip_cache = request.method == "POST"
+    result = resolver.query(ip_address, fields, skip_cache)
+    include_fetch_date = True if fields and "fetched_at" in fields else False
+    include_data_source = True if fields and "data_source" in fields else False
+    
+    return app.response_class(
+        response=json.dumps(response_to_dict(result, include_fetch_date, include_data_source), default=str),
+        status=200,
+        mimetype='application/json'
+    )
+
+@app.route("/batch", methods=["POST"])
+def endpoint_batch():
+    fields = request.args.get("fields", None)
+    fields = fields.split(",") if fields else COLLECTED_FIELDS
+    ip_addresses = request.json
+    results = resolver.query(ip_addresses, fields)
+    include_fetch_date = True if fields and "fetched_at" in fields else False
+    include_data_source = True if fields and "data_source" in fields else False
+    
+    results = [response_to_dict(x, include_fetch_date, include_data_source) for x in results]
+    return app.response_class(
+        response=json.dumps(results, default=str),
+        status=200,
+        mimetype='application/json'
+    )
+
+if __name__ == '__main__':
+    app.run(host=LISTEN_ADDRESS, port=APP_PORT)
